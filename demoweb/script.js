a8a0c1a13e90d3e5e58ce36a88074532a5104d0a
dmerrell <davidpmerrell@gmail.com>
2016-03-10 20:10:35 -0800

    New template from html5boilerplate

--- demoweb/script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("foot01").innerHTML = "<p>&copy; " + new Date().getFullYear() + " David Merrell. All rights reserved.</p>";
-
-document.getElementById("nav01").innerHTML = 
-"<ul id='menu'>" + 
-"<li><a href='index.html'>Home</a></li>" + 
-"<li><a href='customers.html'>Data</a></li>" + 
-"<li><a href='about.html'>About</a></li>" + 
-"</ul>";
